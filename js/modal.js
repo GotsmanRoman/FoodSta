a36960d4d52c902923d789708ddc6a9c875271cb
Gotsman Roman <gotsmanroman@gmail.com>
2022-12-14 23:08:08 +0200

    Додає модальне вікно

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,17 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
+      openModalBtn: document.querySelectorAll("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       backdrop: document.querySelector("[data-backdrop]"),
       modal: document.querySelector("[data-modal]"),
     };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
+
+  refs["openModalBtn"][0].addEventListener("click", toggleModal);
+  refs["openModalBtn"][1].addEventListener("click", toggleModal);
+  refs["openModalBtn"][2].addEventListener("click", toggleModal);
+
+    //refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
